7818bf06a69a050d22981eb2594f13af226ba82d
Abdullah Al Kafi <abdullacst11@gmail.com>
2026-07-28 11:25:52 +0600

    first commit and last module samarey

---
+++ variable.js
@@ -0,0 +1,33 @@
+/**
+ * variable ==> container
+ */
+
+// number
+var sunglassPrice = 80;
+
+//string
+var subject = "CSE";
+
+// boolean
+var passed = true;
+
+/**
+ * Naming Convention
+ * 1. single word
+ * 2. no quote
+ * 3. no gap or dash
+ * 4. can not start with number
+ * 5. can not be a keyword
+ * 6. prefer camelCase
+ */
+
+var lastExamResult = 80;
+
+/**
+ * Operation
+ * 1. +, -, *, /, %
+ * 2. +=, -=, *=, /=
+ * 3. isNaN
+ * 4. null
+ * 5. Number
+ */
