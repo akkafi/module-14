3e67606821114f3c37114fe4d498f848f799c9ed
Abdullah Al Kafi <abdullacst11@gmail.com>
2026-07-28 15:45:34 +0600

    multiple condition logical operator

---
+++ multiple.js
@@ -0,0 +1,45 @@
+const salary = 20000;
+const isBCS = true;
+const height = 78;
+const hasCar = false;
+
+// if(salary > 15000 && height > 66){
+//     console.log("Su------patro");
+// }
+// else{
+//     console.log("Onno Patro khuji")
+// }
+
+// if(salary > 25000 || height > 72){
+//     console.log("eso baba kobul")
+// }
+// else{
+//     console.log("vag tui mokbul")
+// }
+
+//more and more condition
+
+// if(salary > 25000 || height > 72 || isBCS == true){
+//     console.log("eso baba kobul")
+// }
+// else{
+//     console.log("vag tui mokbul")
+// }
+
+// if(salary > 25000 && height > 72 && isBCS == true){
+//     console.log("eso baba kobul")
+// }
+// else{
+//     console.log("vag tui mokbul")
+// }
+
+
+// ----------------COMPLEX COndition-----------------
+
+if((salary > 20000 && hasCar == true) || isBCS == true){
+    console.log("Tomar 14 gusti raji")
+}
+
+if((salary > 20000 || hasCar == true) && isBCS == true){
+    console.log("Tomar 14 gusti raji")
+}
