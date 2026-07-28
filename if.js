8224f313c739e0cb8439e74f1e6b9e6598201f09
Abdullah Al Kafi <abdullacst11@gmail.com>
2026-07-28 12:23:51 +0600

    if else condition

--- if.js
+++ if.js
@@ -27,6 +27,31 @@
  */
 
 
-if(3  < 10){
+if(3  > 10){
     console.log("I have a smaller value")
+}
+
+var briyaniPrice = 250;
+
+if(briyaniPrice < 300){
+    console.log("Mama give me some briyani")
+}
+
+
+/**
+ * if (condition){
+ *  execute code if condition above is true
+ * }
+ * else{
+ *  execute code if condition above is false
+ * }
+ */
+
+var weight = 20;
+
+if(weight <= 20){
+    console.log("I will carry it by Myself")
+}
+else{
+    console.log("I will rent a rickshaw")
 }
