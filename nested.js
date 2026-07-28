ac2e13ea857a8d8ac9696f0c0ac493268b30d0b6
Abdullah Al Kafi <abdullacst11@gmail.com>
2026-07-28 16:44:32 +0600

    nested if-else condition

---
+++ nested.js
@@ -0,0 +1,17 @@
+const money = -1;
+if(money > 300){
+    console.log("Bro you are rich!!!!")
+}
+else{
+    if(money > 100){
+        console.log('tui gorib o  na borolok o na')
+    }
+    else{
+        if(money > 0){
+            console.log('dosto kola kha calcium bara')
+        }
+        else{
+            console.log("tui amr bondu na")
+        }
+    }
+}
