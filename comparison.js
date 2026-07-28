801d131f55c70584cdd94d7c27c727285b7adcb9
Abdullah Al Kafi <abdullacst11@gmail.com>
2026-07-28 12:10:27 +0600

    comparison and if condition

---
+++ comparison.js
@@ -0,0 +1,39 @@
+// console.log(5 > 10);
+// console.log(5 < 10);
+// console.log(5 == 10);
+// console.log(10 == 10);
+// console.log(10 >= 10);
+// console.log(15 >= 10);
+// console.log(5 >= 10);
+// console.log(5 <= 10);
+// console.log(10 <= 10);
+// console.log(100 <= 10);
+
+
+// console.log(10 == 10);
+// console.log(10 == 10.1);
+
+
+//---------------------------------
+var payaraPrice = 40;
+var applePrice = 450;
+
+// console.log(payaraPrice > applePrice);
+// console.log(applePrice < payaraPrice);
+
+
+/**
+ *  ==
+ * ===
+ */
+
+// console.log(10 == 10);
+// console.log(10 == '10');
+// console.log(10 === '10');
+
+
+// console.log(10 === 'tomato');
+
+//------------------------------
+console.log(10 != '10');
+console.log(10 !== '10');
---
+++ if.js
@@ -0,0 +1,32 @@
+/**
+ * tui jodi amake khawas 
+ * taile ami asbo.
+ * 
+ * ------------------------
+ * 
+ * if you by food for me
+ *          i will come
+ * 
+ * ------------------------
+ * 
+ * if briyani price less then 300 taka
+ *                         i will  buy
+ * 
+ * ---------Structure---------------
+ * 
+ * if(condition){
+ * 
+ * }
+ * 
+ * 
+ * 
+ * --------------------------
+ * if(condition){
+ * 
+ * }
+ */
+
+
+if(3  < 10){
+    console.log("I have a smaller value")
+}
