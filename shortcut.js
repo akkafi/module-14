821af1ac9e5cc425fec7626807dac96fa1cbdaa9
Abdullah Al Kafi <abdullacst11@gmail.com>
2026-08-04 07:19:56 +0600

    advanced Logical Not operator

---
+++ shortcut.js
@@ -0,0 +1,19 @@
+// const isLeader = true;
+// const isLeader = false;
+// if(isLeader === true)
+// if(isLeader){
+//     console.log("Leader asche! Chair charo");
+// }
+// else{
+//     console.log("Vitore Dhukte dibina");
+// }
+
+// const isPassed = false;
+// if(isPassed === false)
+// if(!isPassed){
+//     console.log("Toke biya diye dimu");
+// }
+// else{
+//     console.log("Toke biya diye dimu na");
+// }
+
